2c0df0321f9325287545e83a898bf2decd2fced0
半片叶 <34050019+HalfLeaf@users.noreply.github.com>
2020-08-26 09:02:58 +0800

    更新每日一题

---
+++ docs/python/字符串操作/字符串的排列-2020-08-27.py
@@ -0,0 +1,11 @@
+#-*-coding:utf-8-*-
+
+from typing import List
+class Solution:
+    def permutation(self, s: str) -> List[str]:
+        if not s: return []
+
+if __name__ == '__main__':
+    s = Solution()
+    result = s.permutation("abc")
+    print(result)
