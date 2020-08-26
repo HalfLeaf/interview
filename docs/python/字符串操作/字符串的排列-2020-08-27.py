dfef67ad929deeffb8148b1a59cd739efb3f08b0
半片叶 <34050019+HalfLeaf@users.noreply.github.com>
2020-08-26 09:36:36 +0800

    更新题解

--- docs/python/字符串操作/字符串的排列-2020-08-27.py
+++ docs/python/字符串操作/字符串的排列-2020-08-27.py
@@ -1,9 +1,13 @@
 #-*-coding:utf-8-*-
 
 from typing import List
+import itertools
 class Solution:
-    def permutation(self, s: str) -> List[str]:
-        if not s: return []
+    def permutation(self, ss: str) -> List[str]:
+        if not ss:
+            return []
+        return sorted(list(set(map(''.join, itertools.permutations(ss)))))
+
 
 if __name__ == '__main__':
     s = Solution()
