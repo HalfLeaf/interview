6987cc847e8c4d793ab9cb8caf7768caa7aab391
HalfLeaf <ye944205791@163.com>
2020-09-13 13:45:45 +0800

    更新题解

--- docs/python/列表/合并区间-2020-08-31.py
+++ docs/python/列表/合并区间-2020-08-31.py
@@ -2,27 +2,70 @@
 
 from typing import List
 
+class Solution1:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        result = []
+        max_value = None
+        min_value = None
+        if not intervals:
+            return []
+        for index, single in enumerate(intervals):
+            print(single, min_value, max_value)
+            if min_value is None:
+                min_value = single[0]
+                max_value = single[1]
+            elif min_value <= single[0] <= max_value or \
+                 min_value <= single[1] <= max_value or \
+                 single[0] <= min_value <= single[1] or \
+                 single[0] <= max_value <= single[1]:
+                min_value = min(min_value, single[0])
+                max_value = max(max_value, single[1])
+            else:
+                result.append([min_value, max_value])
+                min_value = single[0]
+                max_value = single[1]
+        result.append([min_value, max_value])
+        return result
+
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         result = []
         max_value = None
         min_value = None
-        for single in intervals:
-            flag = 0
-            for one in single:
-                if min_value and max_value and min_value <= one <= max_value:
-                    flag = 1
-                if max_value is None:
-                    max_value = one
-                elif min_value is None:
-                    min_value = one
-                if min_value and min_value > max_value:
-                    min_value, max_value = max_value, min_value
-                if flag:
-                    max_value = max(max_value, one)
-                    result.append([min_value, max_value])
-                    min_value = None
-                    max_value = None
+        if not intervals:
+            return []
+        total = len(intervals)
+        a = None
+        b = None
+        for index, single in enumerate(intervals):
+            if a is None:
+                a = single[0]
+                b = single[1]
+            for i in range(index, total):
+                if min_value is None:
+                    min_value = intervals[i][0]
+                    max_value = intervals[i][1]
+                elif min_value <= intervals[i][0] <= max_value or \
+                     min_value <= intervals[i][1] <= max_value or \
+                     intervals[i][0] <= min_value <= intervals[i][1] or \
+                     intervals[i][0] <= max_value <= intervals[i][1]:
+                    min_value = min(min_value, intervals[i][0])
+                    max_value = max(max_value, intervals[i][1])
+            if min_value <= a <= max_value or \
+               min_value <= b <= max_value or \
+               a <= min_value <= b or \
+               a <= max_value <= b:
+                a = min(min_value, a)
+                b = max(max_value, b)
+                min_value = None
+                max_value = None
+            else:
+                result.append([a, b])
+                min_value = None
+                max_value = None
+                a = single[0]
+                b = single[1]
+        result.append([a, b])
         return result
 
 
@@ -30,5 +73,5 @@ class Solution:
 
 if __name__ == '__main__':
     s = Solution()
-    a = s.merge([[1,4],[4,5]])
+    a = s.merge([[1,3],[4,5],[6,8], [1, 10]])
     print(a)
