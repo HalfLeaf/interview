632bc37d5092294012efd550468510481a104c5a
半片叶 <ye944205791@163.com>
2020-09-14 16:11:56 +0800

    更新题库

--- docs/python/字符串操作/最长公共前缀-2020-09-03.py
+++ docs/python/字符串操作/最长公共前缀-2020-09-03.py
@@ -1,12 +1,25 @@
 #-*-coding:utf-8-*-
 
-from typing import List,Dict
+from typing import List
 
 class Solution:
-    def findItinerary(self):
-        pass
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        length = [len(x) for x in strs]
+        min_str = strs[length.index(min(length))]
+        result = ""
+        for i in range(len(min_str)):
+            flag = 1
+            for s in strs:
+                if s[i] != min_str[i]:
+                    break
+            else:
+                result = f"{result}{min_str[i]}"
+                flag = 0
+            if flag:
+                break
+        return result
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.findItinerary())
+    print(s.longestCommonPrefix(["flower","flow","flerkfight"]))
