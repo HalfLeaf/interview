b4b60c9ee01fb8a46e53bc7132612f3b1bdb7289
半片叶 <ye944205791@163.com>
2020-09-15 09:07:14 +0800

    更新题库

--- docs/python/字符串操作/翻转字符串里的单词-2020-09-05.py
+++ docs/python/字符串操作/翻转字符串里的单词-2020-09-05.py
@@ -2,24 +2,13 @@
 
 import re
 
-def get_max_string(s: str) -> int:
-    result = ""
-    tmp = ""
-    for index, single in enumerate(s):
-        if re.search(r"[0-9]", single):
-            tmp = f"{tmp}{single}"
-        else:
-            if len(tmp) >= len(result):
-                result = tmp
-            tmp = ""
-
-    if len(tmp) >= len(result):
-        result = tmp
-
-    print("".join(result))
-    print(len(result))
-    return len(result)
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s = re.sub(r' +', " ", s.strip()).split(" ")
+        s.reverse()
+        return " ".join(s)
 
 
 if __name__ == '__main__':
-    get_max_string("abcdabc")
+    s = Solution()
+    print(s.reverseWords("  hello       world!  "))
