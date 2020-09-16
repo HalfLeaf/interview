cd77a6b9dca5be8f59bf438ed1aa48d4eb45f2c6
半片叶 <ye944205791@163.com>
2020-09-16 09:21:40 +0800

    更新题库

--- docs/python/双指针/两数之和-2020-09-09.py
+++ docs/python/双指针/两数之和-2020-09-09.py
@@ -3,10 +3,13 @@
 from typing import List,Dict
 
 class Solution:
-    def findItinerary(self):
-        pass
+    def arrayPairSum(self, nums: List[int]) -> int:
+        if len(nums) % 2:
+            return 0
+        nums = sorted(nums)
+        return sum([x for i,x in enumerate(nums) if i%2==0])
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.findItinerary())
+    print(s.arrayPairSum([1,4,3,2]))
