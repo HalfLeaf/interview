52f339d7b81b5e1ed07056e38bc709182e897ae8
半片叶 <ye944205791@163.com>
2020-09-16 17:28:35 +0800

    更新题解

--- docs/python/双指针/最大连续1的个数-2020-09-11.py
+++ docs/python/双指针/最大连续1的个数-2020-09-11.py
@@ -4,6 +4,23 @@ from typing import List,Dict
 
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        total = len(nums)
+        i = 0
+        j = 0
+        result = 0
+        while j < total:
+            if nums[j] == 1:
+                j = j + 1
+            else:
+                result = max(result, j-i)
+                i = j
+                
+        return result
+                
+                
+                
+            
+        
 
 if __name__ == '__main__':
     s = Solution()
