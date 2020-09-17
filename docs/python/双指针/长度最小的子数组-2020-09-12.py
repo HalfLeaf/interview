8d3c5525129dcbd6e25d559e88664464a6fa3230
半片叶 <ye944205791@163.com>
2020-09-17 09:56:00 +0800

    更新题库

--- docs/python/双指针/长度最小的子数组-2020-09-12.py
+++ docs/python/双指针/长度最小的子数组-2020-09-12.py
@@ -1,12 +1,13 @@
 #-*-coding:utf-8-*-
 
-from typing import List,Dict
+from typing import List
 
 class Solution:
-    def findItinerary(self):
-        pass
+    class Solution:
+        def minSubArrayLen(self, s: int, nums: List[int]) -> int:
+
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.findItinerary())
+    print(s.minSubArrayLen(s = 7, nums = [2,3,1,2,4,3]))
